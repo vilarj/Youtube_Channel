86aecf7c528d4f66cc890bc18ab137dd04e7b6d8
vilarj <lamaravillasan@outlook.com>
2020-03-08 12:48:27 -0700

    Par o impar

---
+++ CS1/src/NumeroPar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/*
+ * Escribe un programa que lee un entero desde el usuario
+ * y determina si el entero es par o impar.
+ */
+public class NumeroPar {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n;
+
+        // Adquiriendo el valor desde el usuario
+        System.out.printf("Introduzca el numero: ");
+        n = input.nextInt();
+
+        // Chequeando si el numero es par o no
+        if ( (n % 2) == 0) {
+            System.out.format ("%n es par");
+        }
+
+        else {
+            System.out.print(n + " es impar");
+        }
+
+        // cerrando la clase Scanner
+        input.close();
+    }
+}
