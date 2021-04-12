9ca7da5b16b75bb2969c1ed5b0a34ae399d4ca00
Vilarj <lamaravillasan@outlook.com>
2021-04-12 13:36:33 -0700

    Ejemplo del For Loop

---
+++ CS1/src/For_Menu.java
@@ -0,0 +1,34 @@
+/**
+ Escribe un programa que imprima uno de los siguientes patrones:
+
+ i)
+ **********
+ **********
+ **********
+ **********
+
+ ii)
+ *
+ **
+ ***
+ ****
+ *****
+
+ iii)
+ *
+ ***
+ *****
+ *******
+ *********
+ */
+
+public class For_Menu {
+    public static void main(String[] args) {
+        String objeto = "*";
+
+        for (int k = 5; k > 0; k--) { // i = 0 | proceso | actualizas
+            System.out.println(objeto);
+            objeto += "*"; // objeto = objeto + "*" <-> objeto += "*"
+        }
+    }
+}
